e743c8a76e335e452ae3197e39bac59b742ef18e
Shamim Ahmed <shamim.ahmed@sandstone.com.au>
2018-12-18 02:12:33 +1100

    Add an example on terminal operations for Streams

---
+++ ch04-examples/src/main/java/edu/buet/cse/ch04/examples/stream/TerminalOperationTrial.java
@@ -0,0 +1,26 @@
+package edu.buet.cse.ch04.examples.stream;
+
+import java.util.stream.IntStream;
+
+public class TerminalOperationTrial {
+  public static void main(String... args) {
+    // example 1
+    IntStream stream1 = IntStream.of(1, 3, 5, 7, 9);
+    boolean result1 = stream1.anyMatch(n -> n % 2 == 0);
+    System.out.println(result1);
+
+    System.out.println();
+
+    // example 2
+    IntStream stream2 = IntStream.of(1, 3, 5, 7, 9);
+    boolean result2 = stream2.allMatch(n -> n % 2 != 0);
+    System.out.println(result2);
+
+    System.out.println();
+
+    // example 3
+    IntStream stream3 = IntStream.of(1, 3, 5, 7, 9);
+    boolean result3 = stream3.noneMatch(n -> n % 2 == 0);
+    System.out.println(result3);
+  }
+}
