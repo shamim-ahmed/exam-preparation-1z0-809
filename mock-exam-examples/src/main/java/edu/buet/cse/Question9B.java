8d446201614b4b5114db0fbefefad0d36b5519fa
Shamim Ahmed <shamim.buet.99@gmail.com>
2019-01-14 00:55:38 +1100

    Rename some classes

--- mock-exam-examples/src/main/java/edu/buet/cse/Question9B.java
+++ mock-exam-examples/src/main/java/edu/buet/cse/Question9B.java
@@ -3,7 +3,7 @@ package edu.buet.cse;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class Question8B {
+public class Question9B {
   public static void main(String... args) {
     Path path = Paths.get("/home/shamim/personal/./docs/../temp/hello.txt");
     Path result = path.normalize();
--- mock-exam-examples/src/main/java/edu/buet/cse/Question9C.java
+++ mock-exam-examples/src/main/java/edu/buet/cse/Question9C.java
@@ -3,9 +3,9 @@ package edu.buet.cse;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class Question8A {
+public class Question9C {
   public static void main(String...args) {
-    Path path1 = Paths.get("/home/shamim/personal/temp/hello.txt");
+    Path path1 = Paths.get("/home/shamim/personal/./docs/../temp/hello.txt");
     Path path2 = Paths.get("/home/shamim/personal/temp/poem.txt");
     
     Path result = path1.relativize(path2);
