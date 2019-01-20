5edbcc0efa4fc22aa939fdc12d8f9afe8ea97388
Shamim Ahmed <shamim.ahmed@sandstone.com.au>
2019-01-20 15:32:43 +1100

    Add examples related to performance improvement

---
+++ ch07-examples/src/main/java/edu/buet/cse/ch07/examples/performance/WhaleDataCalculator.java
@@ -0,0 +1,43 @@
+package edu.buet.cse.ch07.examples.performance;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class WhaleDataCalculator {
+  AtomicInteger counter = new AtomicInteger(0);
+
+  public void processAllData(List<Integer> valueList) {
+    valueList.stream().map(n -> processRecord(n)).forEach(n -> counter.addAndGet(n));
+  }
+
+  public int getCounterValue() {
+    return counter.get();
+  }
+
+  private Integer processRecord(Integer val) {
+    try {
+      Thread.sleep(10L);
+    } catch (InterruptedException ex) {
+      System.err.println(ex);
+    }
+
+    return val + 1;
+  }
+
+  public static void main(String... args) {
+    WhaleDataCalculator calculator = new WhaleDataCalculator();
+    List<Integer> valueList = new ArrayList<>();
+
+    for (int i = 0; i < 4000; i++) {
+      valueList.add(i);
+    }
+
+    long start = System.currentTimeMillis();
+    calculator.processAllData(valueList);
+    long end = System.currentTimeMillis();
+
+    System.out.printf("Processing time = %.3f s%n", (end - start) / 1000.0);
+    System.out.printf("counter value = %d%n", calculator.getCounterValue());
+  }
+}
